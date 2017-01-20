46f62399a7c95a035a23bbb774b5a065b8de24c7
Jeffrey Barnes <JBARNES@gmelogin.com>
2017-01-20 15:23:59 -0600

    initial

---
+++ main.js
@@ -0,0 +1,20 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+	var path = require('path');
+	var userName = process.env['USERPROFILE'].split(path.sep)[2];
+
+	// Send the HTTP header
+	// HTTP Status: 200 : OK
+	// Content Type: text/plain
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+
+	// Send the response body as "Hello World"
+	response.end('Hello ' + userName + ' welcome to the Node.JS World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+
+//TODO: Add child process to fire the browser
